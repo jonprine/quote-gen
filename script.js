f6d26509fe112d5af7c05b10dc6f87e1716e45be
Vox Humana <jonprine@gmail.com>
2021-01-07 17:41:41 -0600

    quote to ui

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn= document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // get quote from api
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        var response = await fetch(proxyUrl + apiUrl);
-        var data = await response.json();
-        console.log(data);
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // if author is blank, add 'uknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // reduce font size for long quotes
+        if (data.quoteText.length > 50) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerText = data.quoteText;
     } catch (error) {
         getQuote();
-        console.log('whoops, no quote', error);
     }
 
 }
