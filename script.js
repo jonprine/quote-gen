102898bd4481b8c4ef8131e356df57e5cbb2b719
Vox Humana <jonprine@gmail.com>
2021-01-07 17:16:56 -0600

    fixed api call

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // get quote from api
 async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
+        var response = await fetch(proxyUrl + apiUrl);
+        var data = await response.json();
         console.log(data);
     } catch (error) {
         console.log('whoops, no quote', error);
     }
+
 }
 
 // on load
